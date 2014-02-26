234f998bb17c8aeb377ae6773aea769ee2eefb41
Ashiq A. <ashes999@yahoo.com>
2014-02-26 12:07:44 -0500

    Curses FPS Test code
    Resolves ~30+ FPS. Changing colour is the major killer (using a single
    colour nets around 60FPS).

--- test.rb
+++ test.rb
@@ -8,11 +8,35 @@ Curses.stdscr.keypad(true) # enable arrow keys (required for pageup/down)
 Curses.start_color
 
 for n in (0 .. 15) do		
-	Curses.init_pair(n, n, 1)
-	
-	Curses.attron(color_pair(n)|A_NORMAL){
-		Curses.addstr("#")
-    }
+	Curses.init_pair(n, n, 0)
 end
 
-Curses.getch
+start = Time.new
+duration = Time.new - start
+draws = 0
+
+while (duration <= 10) do
+	
+	for y in (0 .. 25) do
+		for x in (0 .. 80) do
+		
+			color = y % 16 #rand(0..15)
+			letter = ('a'..'z').to_a.shuffle[0]
+			
+			Curses.attron(color_pair(color)|A_NORMAL){
+				setpos(y, x)
+				addch(letter)
+			}
+		end
+	end
+	
+	setpos(0, 0)
+	addstr("#{draws/duration} FPS ")
+	
+	refresh
+	draws += 1
+	
+	duration = Time.new - start	
+end
+
+puts "Completed in #{duration} seconds with #{draws/duration} FPS"
