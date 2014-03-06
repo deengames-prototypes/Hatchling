e4265a4ee507e597bc9f9f64a8e1baff07bead77
Ashiq A <ashes999@yahoo.com>
2014-03-06 11:14:42 -0500

    Fix publish script to include data files.

--- publish.rb
+++ publish.rb
@@ -1,7 +1,15 @@
+includes = ['assets', 'data']
+
 require 'fileutils'
 FileUtils.rm_rf('release')
 Dir.mkdir('release')
-system('ocra main.rb')
-FileUtils.cp('main.exe', 'release')
-FileUtils.copy_entry('assets', 'release/assets')
-File.delete('main.exe')
+
+system('ocra lib/hatchling.rb')
+FileUtils.cp('hatchling.exe', 'release')
+File.delete('hatchling.exe')
+
+includes.each do |i|
+	FileUtils.copy_entry(i, "release/#{i}")
+end
+
+puts 'Done. Release is published to /release.'
