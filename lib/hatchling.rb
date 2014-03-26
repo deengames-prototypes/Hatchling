c81404e80a3216987f8b8f733953d472b2035b6d
Ashiq A <ashes999@yahoo.com>
2014-03-26 13:06:05 -0400

    Isolate Hatchling gem from game code

--- lib/hatchling.rb
+++ lib/hatchling.rb
@@ -1,37 +1,41 @@
-require 'json'
+class Hatchling
+	def self.start
+		require 'json'
 
-profile = false
+		profile = false
 
-# Load the game.
-if (!File.exists?('data/game.json')) then
-	raise 'Missing main game definition file: data/game.json'
-end
+		# Load the game.
+		if (!File.exists?('data/game.json')) then
+			raise 'Missing main game definition file: data/game.json'
+		end
 
-game_data = JSON.parse(File.read('data/game.json'))
-game_name = game_data['name']
+		game_data = JSON.parse(File.read('data/game.json'))
+		game_name = game_data['name']
+					
+		require_relative 'utils/logger'
+		require_relative 'game'
+		Logger.init(game_name)
+		Logger.info('Starting game ...')
+
+		if profile
+			result = RubyProf.profile do
+				Game.new.start
+			end
 			
-require_relative 'utils/logger'
-require_relative 'game'
-Logger.init(game_name)
-Logger.info('Starting game ...')
+			File.open('flat.txt', 'w') do |f|
+				printer = RubyProf::FlatPrinter.new(result)
+				printer.print(f)
+			end
 
-if profile
-	result = RubyProf.profile do
-		Game.new.start
-	end
-	
-	File.open('flat.txt', 'w') do |f|
-		printer = RubyProf::FlatPrinter.new(result)
-		printer.print(f)
-	end
+			File.open('graph.txt', 'w') do |f|
+				printer = RubyProf::GraphPrinter.new(result)
+				printer.print(f, {})
+			end
 
-	File.open('graph.txt', 'w') do |f|
-		printer = RubyProf::GraphPrinter.new(result)
-		printer.print(f, {})
-	end
+		else
+			Game.new.start
+		end
 
-else
-	Game.new.start
+		Logger.info('Goodbye!');
+	end
 end
-
-Logger.info('Goodbye!');
