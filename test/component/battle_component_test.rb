d2ec78b1a3003cf529b1104282d25b8aceba24c7
Ashiq A <ashes999@yahoo.com>
2014-03-27 17:25:34 -0400

    Fix bug with player generating off stairs. Also try to fix stairs.

--- test/component/battle_component_test.rb
+++ test/component/battle_component_test.rb
@@ -4,13 +4,13 @@ require_relative "#{SOURCE_ROOT}/component/battle_component"
 class BattleComponentTest < Test::Unit::TestCase
 	
 	def test_initialize_sets_strength_and_speed
-		b = BattleComponent.new(17, 241)
+		b = BattleComponent.new(17, 241, nil)
 		assert_equal(17, b.strength)
 		assert_equal(241, b.speed)
 	end
 	
 	def test_initialize_raises_if_values_are_non_positive_integers
-		assert_raise(RuntimeError) { BattleComponent.new(0, "hi") }
-		assert_raise(RuntimeError) { BattleComponent.new(13, -1) }		
+		assert_raise(RuntimeError) { BattleComponent.new(0, "hi", nil) }
+		assert_raise(RuntimeError) { BattleComponent.new(13, -1, nil) }		
 	end
 end
--- test/test_config.rb
+++ test/test_config.rb
@@ -1,5 +1,7 @@
 # Used to keep relative paths DRY
 require 'test/unit'
 TEST_ROOT = Dir.pwd
-Dir.chdir("#{Dir.pwd}/../lib")
+Dir.chdir("#{Dir.pwd}/../lib/hatchling")
 SOURCE_ROOT = Dir.pwd
+
+require_relative '../lib/hatchling'
