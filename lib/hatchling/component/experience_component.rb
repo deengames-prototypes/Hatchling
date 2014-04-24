481f41651ebb5e6b24747bb89526a3e81af86bf9
Ashiq A <ashes999@yahoo.com>
2014-04-24 17:08:33 -0400

    Add missing component

---
+++ lib/hatchling/component/experience_component.rb
@@ -0,0 +1,29 @@
+require_relative 'base_component'
+
+class ExperienceComponent < BaseComponent
+	attr_reader :experience, :level	
+	
+	def initialize(exp_for_level_lambda)
+		@exp_for_level = exp_for_level_lambda
+		@experience = 0
+		@level = 1
+	end
+	
+	def next_level_at
+		return @exp_for_level.call(@level + 1)
+	end
+	
+	def gain_experience(points)
+		raise "Please specify numerical experience points (not: #{points})" if !['Float', 'Fixnum'].include?(points.class.name)
+		raise "Please specify a positive amount of experience points" if points <= 0
+		
+		@experience += points
+		old_level = @level
+		
+		while (@experience >= next_level_at) do
+			@level += 1
+		end
+		
+		#Hatchling::EventSystem.trigger('Levelled up', { :Level => @level, :levels_up  => @level - old_level })
+	end
+end
