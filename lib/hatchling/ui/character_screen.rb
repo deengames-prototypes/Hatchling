993b4ef0cfc85b5133ea2e40ac11fe309140f3c7
ashes999 <ashes999@yahoo.com>
2014-05-27 14:35:35 -0400

    Just a comment

--- lib/hatchling/ui/character_screen.rb
+++ lib/hatchling/ui/character_screen.rb
@@ -8,6 +8,7 @@ class CharacterScreen
 	def show		
 		@display_system.clear
 		@input_system.get_input
+		# Not a hack. Entities will be drawn over these.
 		@display_system.fill_screen('.', Color.new(128, 128, 128))
 		@display_system.draw
 	end
